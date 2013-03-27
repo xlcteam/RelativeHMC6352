89212b455cd78a7107e908893ee905fb1870de64
mr.Shu <mr@shu.io>
2013-03-27 15:43:22 +0100

    added example

---
+++ RelativeHMC6352/examples/RelativeHMC6352_demo/RelativeHMC6352.ino
@@ -0,0 +1,18 @@
+
+#include <RelativeHMC6352.h>
+
+
+RelativeHMC6352 compass = RelativeHMC6352();
+
+void setup ()
+{
+  Serial.begin(115200);
+  compass.set_north();
+
+}
+
+void loop ()
+{
+  Serial.println(compass.angle());
+  delay(20);
+}
